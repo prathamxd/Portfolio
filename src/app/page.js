918727e534ccd91afce6b274c28677c26c84c789
Pratham <prathamkl17@gmail.com>
2023-10-25 21:40:46 +0530

    added article project 2

--- src/app/page.js
+++ src/app/page.js
@@ -151,7 +151,7 @@ const Home = () => {
                     <p>React</p>
                     <p>Tailwind CSS</p>
                     <p>Redux Toolkit</p>
-                    <p>OpenAPI API</p>
+                    <p>API</p>
                   </div>
                 </div>
                 <div className="flex gap-3 lg:gap-4">
