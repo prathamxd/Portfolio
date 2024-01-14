cde8a9d84e58342d12742c61b01daa21ee68df2b
Pratham <prathamkl17@gmail.com>
2024-01-14 20:39:12 +0530

    resume updated v12

--- src/app/page.js
+++ src/app/page.js
@@ -39,10 +39,10 @@ const Home = () => {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1Iq0x-i-Aeblr3h4LA1BQFlXaYv1vM7OZ/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1HQBdHl6jFoBjXkD2sK5So-Xrybpn1_e0/view?usp=sharing"
                   target="_blank"
                 >
-                  Resume 
+                  Resume
                 </a>
               </li>
             </ul>
